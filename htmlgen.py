512df32cd31bef089b2e1a254077def5aaa60ae8
SoyZatarain <jarztv@gmail.com>
2019-02-07 19:44:26 -0700

    Simbolizador para etiquetas terminado

--- htmlgen.py
+++ htmlgen.py
@@ -138,4 +138,39 @@ class HTMLGEN:
         "wbr":        SIMBOLIZAR("HTML5NW_WBR",        "WBR"),        # XXX Defines a possible line-break
     }
 
-    
+    def __init__(self, texto):
+        self.texto = texto
+        self.posicion = 0
+        self.caracterActual = self.texto[self.posicion]
+        self.caracterAnterior = None
+        self.columna = 0
+        self.linea = 1
+
+    def cargarSiguenteSimbolo(self):
+        while self.caracterActual is not None:
+            if self.caracterActual.isspace():
+                if self.caracterActual == "\n":
+                    self.linea += 1
+                while self.caracterActual is not None and self.caracterActual.isspace():
+                    self.posicion += 1
+                    self.columna += 1
+                    if self.posicion > len(self.texto) - 1:
+                        self.caracterActual = None
+                    else:
+                        self.caracterAnterior = self.texto[self.posicion-1]
+                        self.caracterActual = self.texto[self.posicion]
+                self.columna = 1
+                continue
+            elif self.caracterActual.isalpha():
+                cadenaEncontrada = ""
+                while self.caracterActual is not None and self.caracterActual.isalnum():
+                    cadenaEncontrada += self.caracterActual
+                    self.posicion += 1
+                    self.columna += 1
+                    if self.posicion > len(self.texto) - 1:
+                        self.caracterActual = None
+                    else:
+                        self.caracterActual = self.texto[self.posicion]
+                        self.caracterAnterior = self.texto[self.posicion-1]
+            raise Exception("Caracter no reconocido: " + self.caracterActual)
+        return SIMBOLIZAR("FIN", None)
