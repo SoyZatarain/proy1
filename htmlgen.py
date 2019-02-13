f9e30e58ee29feb98c9bef98781e85633e7f1cc1
SoyZatarain <jarztv@gmail.com>
2019-02-13 03:16:19 -0700

    Eliminacion de codigo redundante

--- htmlgen.py
+++ htmlgen.py
@@ -13,42 +13,33 @@ class HTMLGEN:
         self.columna = 0
         self.linea = 1
 
+    def avanzar():
+        self.posicion += 1
+        self.columna += 1
+        if self.posicion > len(self.texto) - 1:
+            self.caracterActual = None
+        else:
+            self.caracterAnterior = self.texto[self.posicion-1]
+            self.caracterActual = self.texto[self.posicion]
+
     def cargarSiguienteSimbolo(self):
         while self.caracterActual is not None:
             if self.caracterActual.isspace():
                 if self.caracterActual == "\n":
                     self.linea += 1
                 while self.caracterActual is not None and self.caracterActual.isspace():
-                    self.posicion += 1
-                    self.columna += 1
-                    if self.posicion > len(self.texto) - 1:
-                        self.caracterActual = None
-                    else:
-                        self.caracterAnterior = self.texto[self.posicion-1]
-                        self.caracterActual = self.texto[self.posicion]
+                    self.avanzar()
                 self.columna = 1
                 continue
             elif self.caracterActual.isalpha():
                 cadenaEncontrada = ""
                 while self.caracterActual is not None and self.caracterActual.isalnum():
                     cadenaEncontrada += self.caracterActual
-                    self.posicion += 1
-                    self.columna += 1
-                    if self.posicion > len(self.texto) - 1:
-                        self.caracterActual = None
-                    else:
-                        self.caracterActual = self.texto[self.posicion]
-                        self.caracterAnterior = self.texto[self.posicion-1]
+                    self.avanzar()
                 return diccionarios.etiquetas.get(cadenaEncontrada)
             elif self.caracterActual in list(diccionarios.simbolos.keys()):
                 temp = self.caracterActual
-                self.posicion += 1
-                self.columna += 1
-                if self.posicion > len(self.texto) - 1:
-                    self.caracterActual = None
-                else:
-                    self.caracterActual = self.texto[self.posicion]
-                    self.caracterAnterior = self.texto[self.posicion-1]
+                self.avanzar()
                 return diccionarios.simbolos.get(temp)
             raise Exception("Caracter no reconocido: " + self.caracterActual)
         return diccionarios.SIMBOLIZAR("FIN", None)
