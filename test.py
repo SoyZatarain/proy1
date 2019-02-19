b0e2ee25d47a96ed11a1734b944a88dd5f9990fd
SoyZatarain <jarztv@gmail.com>
2019-02-19 13:57:15 -0700

    Primera prueba con nuestro milestone. Falta simbolizar los inner que no son etiquetas,

--- test.py
+++ test.py
@@ -1,6 +1,35 @@
 from htmlgen import *
 
-contenido = HTMLGEN('<img src="img.jpg" width="100" height="200">')
+milestone = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form action="/action.php" method="POST">
+        <div>
+            <label for="server">Hostname or ip: </label>
+            <input type="text" name="server" id="server">
+        </div>
+        <div>
+            <label for="user">Username: </label>
+            <input type="text" name="user" id="user">
+        </div>
+        <div>
+            <label for="pass">Password</label>
+            <input type="password" name="pass" id="pass">
+        </div>
+        <input type="submit" value="Log in">
+    </form>
+</body>
+</html>
+"""
+
+contenido = HTMLGEN(milestone)
 
 while contenido.caracterActual is not None:
     nodo = contenido.cargarSiguienteSimbolo()
